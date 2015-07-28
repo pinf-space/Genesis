18c13fbc95da837ebc76cf2a84e14d6b606e276b
Christoph Dorn <christoph@christophdorn.com>
2015-07-27 23:39:47 -0700

    wip

--- api/origin/get-invite-token.js
+++ api/origin/get-invite-token.js
@@ -3,15 +3,15 @@
 require('org.pinf.genesis.lib/lib/api').forModule(require, module, function (API, exports) {
 
 
-	var InviteToken = function () {
+	var Command = function () {
 		this.$PLComponent = "space.pinf.genesis/origin.get-invite-token/0";
 	}
-	InviteToken.prototype.getLabel = function () {
+	Command.prototype.getLabel = function () {
 		return this.label || "PINF.Space.Genesis / Origin / Get invite token";
 	}
-	InviteToken.prototype.run = function () {
+	Command.prototype.run = function () {
 
-//console.log("RUNING INVITE TOKEN!", this);
+console.log("RUNING INVITE TOKEN!", this);
 
 //console.log("INIT INVITE TOKEN!", this);
 
@@ -24,7 +24,7 @@ require('org.pinf.genesis.lib/lib/api').forModule(require, module, function (API
 //console.log("config", config);
 
 		return {
-			"$space.pinf.genesis/origin.get-invite-token/0": API.EXTEND(true, new InviteToken(), config)
+			"$tools.pinf.CloudCommands/command/0": API.EXTEND(true, new Command(), config)
 		};
 	}
 
